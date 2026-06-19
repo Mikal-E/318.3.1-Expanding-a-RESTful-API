9e13df8fbd6a8ff6f7331b47661c574c58f2111c
Mikal-E <melamin101@gmail.com>
2026-06-18 20:23:08 -0400

    Created a comments data file that has an empty array, stored in a comments variable. It is for the POST /comments.

---
+++ data/comments.js
@@ -0,0 +1,17 @@
+/* Part 2: Adding Additional Routes - create a place to store comments.
+
+Empty array below is for the following:
+
+When creating a new comment object, it should have the following fields:
+id: a unique identifier.
+userId: the id of the user that created the comment.
+postId: the id of the post the comment was made on.
+body: the text of the comment. */
+
+const comments = [
+
+
+
+];
+
+export default comments;
